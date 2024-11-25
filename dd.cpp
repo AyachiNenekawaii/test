875580f3a90544abaca6d19bd46aebd7ab9940f5
Juncen Guo <2833758035@qq.com>
2024-11-25 14:58:14 +0800

    The fifth commit

--- dd.cpp
+++ dd.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int a, b, c;
-    std::cin >> a >> b >> c;
+    int a, b, c, d;
+    std::cin >> a >> b >> c >> d;
     std::cout << (a + b) / c << std::endl;
     return 0;
 }
